5b817707c792c7d52602b181a8d3469f185a35c4
cookiebutcher <cookie.butcher@gmail.com>
2017-07-24 23:33:19 +0200

    code fixes - not needed usings

--- DotNetCore/CoreDataRepository/src/CoreDataRepository/Customer.cs
+++ DotNetCore/CoreDataRepository/src/CoreDataRepository/Customer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CoreDataRepository
 {
